aa5c72c41afcad5658c20283307d60cd9d7c449a
Stanley <78428936+chepkok3@users.noreply.github.com>
2024-01-07 13:40:35 +0300

    add products database

---
+++ src/products.js
@@ -0,0 +1,73 @@
+export const PRODUCTS = {
+  product1: {
+    id: 1,
+    productName: 'SAMSUNG Galaxy S23',
+    price: 580,
+    productImage:
+      'https://m.media-amazon.com/images/I/71iQPXoiWYL._AC_SX679_.jpg',
+  },
+  product2: {
+    id: 2,
+    productName: 'Google Pixel 8',
+    price: 549,
+    productImage:
+      'https://m.media-amazon.com/images/I/71SfoZu9a3L.__AC_SX300_SY300_QL70_FMwebp_.jpg',
+  },
+  product3: {
+    id: 3,
+    productName: 'OnePlus Nord N30 5G',
+    price: 298,
+    productImage:
+      'https://m.media-amazon.com/images/I/616uMtIWiiL._AC_SX679_.jpg',
+  },
+  product4: {
+    id: 4,
+    productName: 'OnePlus Open',
+    price: 1499,
+    productImage:
+      'https://m.media-amazon.com/images/I/71slFjJerBL.__AC_SX300_SY300_QL70_FMwebp_.jpg',
+  },
+  product5: {
+    id: 5,
+    productName: 'Nokia G10',
+    price: 99,
+    productImage:
+      'https://m.media-amazon.com/images/I/81NgEMJiUZL._AC_UY436_FMwebp_QL65_.jpg',
+  },
+  product6: {
+    id: 6,
+    productName: 'Moto G Stylus 5G',
+    price: 134,
+    productImage:
+      'https://m.media-amazon.com/images/I/51bfhhUKhTL._AC_UY436_FMwebp_QL65_.jpg',
+  },
+  product7: {
+    id: 7,
+    productName: 'Moto G Stylus 5G',
+    price: 134,
+    productImage:
+      'https://m.media-amazon.com/images/I/51bfhhUKhTL._AC_UY436_FMwebp_QL65_.jpg',
+  },
+  product8: {
+    id: 8,
+    productName: 'SAMSUNG Galaxy Z Flip 5 Cell Phone',
+    price: 874,
+    productImage:
+      'https://m.media-amazon.com/images/I/61IqkfGCw5L._AC_SX679_.jpg',
+  },
+  product9: {
+    id: 9,
+    productName: 'Motorola raz |2023|',
+    price: 499,
+    productImage:
+      'https://m.media-amazon.com/images/I/61nVT+i3rdL._AC_SX679_.jpg',
+  },
+  product10: {
+    id: 10,
+    productName: 'Apple iPad (9th Generation)',
+    price: 249,
+    productImage:
+      'https://m.media-amazon.com/images/I/61NGnpjoRDL._AC_SX679_.jpg',
+  },
+};
+export const productsArray = Object.values(PRODUCTS);
