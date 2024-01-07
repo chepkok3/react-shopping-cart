7e6a60704cf452c4ff27b94cc4745953700f0ed8
Stanley <78428936+chepkok3@users.noreply.github.com>
2024-01-07 13:35:23 +0300

    react-router-dom and phosphor icons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Navbar } from './components/navbar';
+import { Shop } from './pages/shop/shop';
+import { Cart } from './pages/cart/cart';
 
 function App() {
-  return <h1> Hello</h1>;
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
